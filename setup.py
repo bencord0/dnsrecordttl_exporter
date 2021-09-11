012ddef502882975ec45b13b99ecd194cef50349
Ben Cordero <bencord0@condi.me>
2021-09-11 08:39:16 +0100

    v0.1.1: Update dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dnsrecordttl_exporter',
-    version='0.1',
+    version='0.1.1',
     package_dir={'': 'src'},
     install_requires=[
         'dnspython',
